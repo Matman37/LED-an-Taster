5f8d6090066a2776896f17a1d9edf0db4d1b4ccc
Matman37 <71316447+Matman37@users.noreply.github.com>
2020-09-25 13:05:36 +0200

    Add files via upload

---
+++ LED an Taster.ino
@@ -0,0 +1,24 @@
+#define LED 2
+#define Taster 10
+
+boolean eingabe = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(250000);
+
+  pinMode (LED, OUTPUT);
+  pinMode (Taster, INPUT);
+}
+
+void loop() {
+  eingabe = digitalRead(Taster);
+  digitalWrite (LED, eingabe);
+  if (eingabe == 1)
+  {
+    Serial.println("LED an");
+  }
+  else {
+    Serial.println("LED aus");
+  }
+}
